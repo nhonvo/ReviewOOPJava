1d60b575f5d278ff053823e68f4d0be24130d161
truongnhon-hutech <vothuongtruongnhon2002@gmail.com>
2022-04-26 14:05:48 +0700

    chuoi String test

---
+++ chuoiJava.java
@@ -0,0 +1,14 @@
+public class chuoiJava{
+
+    
+    public static void main(String[] args) {
+        // create String a
+        String a = "Hello";
+        // create String b
+        String b = "World";
+        // create String c
+        String c = a + " " + b;
+        // print c
+        System.out.println(c);
+    }
+}
