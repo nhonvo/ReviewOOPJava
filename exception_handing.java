ba6f100e3731d7082934827b61c6462b0c8cad42
Le Hieu Nghia <lehieunghia2402@gmail.com>
2022-04-26 15:12:18 +0700

    five-commit

--- exception_handing.java
+++ exception_handing.java
@@ -2,7 +2,7 @@ public class exception_handing {
     public static void main(String[] args) {
         try {
             Float f1 = 3.0f;
-            int x = f1.intValue(); // cast to int "vietsub ép sang kiểu int"
+            int x = f1.intValue(); // cast to int
             byte b = f1.byteValue();
             double d = f1.doubleValue();
             System.out.println(x + b + d);
---
+++ exception_handing_ex2.java
@@ -0,0 +1,17 @@
+public class exception_handing_ex2 {
+    public static void main(String[] args) {
+        try {
+            obsoleteMethod();
+            System.out.print("A");
+        } catch (Exception ex) {
+            System.out.print("B");
+        } finally {
+            System.out.print("C");
+        }
+        System.out.print("D");
+    }
+
+    public static void obsoleteMethod() {
+        throw new RuntimeException();
+    }
+}
