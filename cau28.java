045565a2db7b133c2c7e2875cb88a0cc2b5ac1b0
truongnhon-hutech <vothuongtruongnhon2002@gmail.com>
2022-04-26 14:19:19 +0700

    cau28 - 27

--- cau28.java
+++ cau28.java
@@ -1,3 +1,9 @@
 public class cau28 {
-    
+    public static void main(String[] args) {
+        boolean x = true, y = false;
+        System.out.println(x && y );
+        // output: false 
+        System.out.println(x && y || !x); 
+        // output : false
+    }
 }
