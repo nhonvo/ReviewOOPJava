bd8ef60cc0bf997e7a4924bda8652d3b5bfda86c
Le Hieu Nghia <lehieunghia2402@gmail.com>
2022-04-26 14:52:32 +0700

    third-commit

---
+++ exception_handing.java
@@ -0,0 +1,13 @@
+public class exception_handing {
+    public static void main(String[] args) {
+        try {
+            Float f1 = 3.0f;
+            int x = f1.intValue();
+            byte b = f1.byteValue();
+            double d = f1.doubleValue();
+            System.out.println(x + b + d);
+        } catch (NumberFormatException e) {
+            System.out.println("Bad number");
+        }
+    }
+}
