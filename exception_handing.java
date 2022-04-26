290dee6b029864adde11c8e32d80fc93e1727646
Le Hieu Nghia <lehieunghia2402@gmail.com>
2022-04-26 15:13:31 +0700

    six-commit

--- exception_handing.java
+++ exception_handing.java
@@ -10,16 +10,18 @@ public class exception_handing {
             System.out.println(x + b + d);
             System.out.println(a);// chuỗi
             System.out.println(aa); // char kí tự
-            System.out.println(a+ x + b + d );
+            System.out.println(a + x + b + d);
             // vì kiểu char gặp double sẽ chuyển sang kiểu double theo bảng mã Ascii
-            // output: 60 
-            System.out.println(a+ x + b + d );
+            // output: 60
+            System.out.println(a + x + b + d);
             // output: 3333
-            System.out.println( x + b + d +a);
+            System.out.println(x + b + d + a);
             // output: 9.03
-            
+
         } catch (NumberFormatException e) {
             System.out.println("Bad number");
         }
     }
 }
+
+// tested;
