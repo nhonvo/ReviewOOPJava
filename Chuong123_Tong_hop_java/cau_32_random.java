582f4f3ee589332262509b3adb41dfbd1bcf9f13
truongnhon-hutech <vothuongtruongnhon2002@gmail.com>
2022-04-26 22:37:10 +0700

    Lap trinh OOP co ban

--- Chuong123_Tong_hop_java/cau_32_random.java
+++ Chuong123_Tong_hop_java/cau_32_random.java
@@ -8,9 +8,13 @@ public class cau_32_random {
     public void randd() {
         Random rd = new Random();
         // int iRand = Math.random();// Lỗi sai cú pháp
-        // int a = rd.nextInt() % 10;
+        double iRand = Math.random();// fix
+        int iRand1 = (int) Math.random();// fix
+
         int a = rd.nextInt(10);
+        // int a = rd.nextInt() % 10;
         // Or iRand = (int) (Math.random() * 10);
+        // output: 1 - 9
         System.out.println(a);
         for (int i = 0; i < 10; i++) {
             System.out.println(rd.nextInt(10));
