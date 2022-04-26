2a76a2381200e79b105811c22e25b30fc6098606
truongnhon-hutech <vothuongtruongnhon2002@gmail.com>
2022-04-26 17:31:38 +0700

    nhon dep trai

---
+++ Generic/Generic_1.java
@@ -0,0 +1,18 @@
+package Generic;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Generic_1 {
+    public static void main(String[] args) {
+        Integer[] arr = { 1, 2, 3, 4, 5 };
+        List<Integer> arrList = fromArrayToList(arr);
+        System.out.println(arrList);
+        // output: [1, 2, 3, 4, 5]
+    }
+
+    public static <T extends Number> List<T> fromArrayToList(T[] a) {
+        return Arrays.stream(a).collect(Collectors.toList());
+    }
+}
---
+++ Generic/Generic_2.java
@@ -0,0 +1,16 @@
+package Generic;
+import java.util.ArrayList;
+
+public class Generic_2 {
+    public static void main(String[] args) {
+        ArrayList<String> userNames = new ArrayList<>();
+        userNames.add("Tom");
+        userNames.add("Jerry");
+        userNames.add(new Integer(100));
+        String userNames1 = userNames.get(0);
+        System.out.println("userNames: " + userNames1);
+        // output : Exception in thread "main" java.lang.Error: Unresolved compilation problem:
+        // The method add(String) in the type ArrayList<String> is not applicable for
+        // the arguments (Integer)
+    }
+}
