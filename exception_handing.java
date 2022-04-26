52b9c4c1e21676ad57a3cbb2bf57360ea04da07b
truongnhon-hutech <vothuongtruongnhon2002@gmail.com>
2022-04-26 15:10:47 +0700

    cau32 random

--- exception_handing.java
+++ exception_handing.java
@@ -5,7 +5,19 @@ public class exception_handing {
             int x = f1.intValue(); // cast to int "vietsub ép sang kiểu int"
             byte b = f1.byteValue();
             double d = f1.doubleValue();
+            String a = "3";
+            char aa = '3';
             System.out.println(x + b + d);
+            System.out.println(a);// chuỗi
+            System.out.println(aa); // char kí tự
+            System.out.println(a+ x + b + d );
+            // vì kiểu char gặp double sẽ chuyển sang kiểu double theo bảng mã Ascii
+            // output: 60 
+            System.out.println(a+ x + b + d );
+            // output: 3333
+            System.out.println( x + b + d +a);
+            // output: 9.03
+            
         } catch (NumberFormatException e) {
             System.out.println("Bad number");
         }
